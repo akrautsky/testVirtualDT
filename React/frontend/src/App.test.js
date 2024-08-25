abea602d4318ddbea18aef395e1d10dab7d76725
“Akash <akraut6501@gmail.com>
2024-08-24 21:47:48 -0400

    Removing test from CI_CD

--- React/frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Order Online text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Order Online/i);
-  expect(linkElement).toBeInTheDocument();
-});
